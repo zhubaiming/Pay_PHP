82c84b67b348bf89d67bf1c8f771c0ca6aec8ef8
zhubaiming <other.zhubaiming@gmail.com>
2025-07-24 05:34:35 +0000

    调整快捷方式调用插件

--- src/Shortcuts/Wechat/TransferCancelShortcut.php
+++ src/Shortcuts/Wechat/TransferCancelShortcut.php
@@ -11,7 +11,7 @@ use Hongyi\Designer\Plugins\ParserPlugin;
 use Hongyi\Designer\Plugins\StartPlugin;
 use Hongyi\Pay\Plugins\Wechat\BeginPlugin;
 use Hongyi\Pay\Plugins\Wechat\V3\AddPayloadSignaturePlugin;
-use Hongyi\Pay\Plugins\Wechat\V3\Marketing\Transfer\BillsPlugin;
+use Hongyi\Pay\Plugins\Wechat\V3\Marketing\Transfer\CancelPlugin;
 use Hongyi\Pay\Plugins\Wechat\V3\VerifySignaturePlugin;
 
 class TransferCancelShortcut implements ShortcutInterface
@@ -21,7 +21,7 @@ class TransferCancelShortcut implements ShortcutInterface
         return [
             BeginPlugin::class,
             StartPlugin::class,
-            BillsPlugin::class,
+            CancelPlugin::class,
             AddBodyToPayloadPlugin::class,
             AddPayloadSignaturePlugin::class,
             AddRadarPlugin::class,
